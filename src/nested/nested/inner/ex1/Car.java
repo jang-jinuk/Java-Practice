2c533e538604422d43322a8f29aa28f0b2943869
jang-jinuk <dlsndifn98@gmail.com>
2024-12-11 21:48:23 +0900

    중첩 클래스, 내부 클래스 - 내부 클래스의 활용
    - Car 클래스 내부에 Engin 클래스를 선언하였다.
    - 내부 클래스 선언으로 불필요하게 노출되었던 인스터스 멤버 변수와 별도로 메서드를 추가하지 않게 되었다.

--- src/nested/nested/inner/ex1/Car.java
+++ src/nested/nested/inner/ex1/Car.java
@@ -8,21 +8,18 @@ public class Car {
   public Car(String model, int chargeLevel) {
     this.model = model;
     this.chargeLevel = chargeLevel;
-    this.engine = new Engine(this);
-  }
-
-  // Engine에서만 사용하는 메서드
-  public String getModel() {
-    return model;
-  }
-
-  // Engine에서만 사용하는 메서드
-  public int getChargeLevel() {
-    return chargeLevel;
+    this.engine = new Engine();
   }
 
   public void start() {
     engine.start();
     System.out.println(model + "시작 완료");
+  }
+
+  public class Engine {
+    public void start() {
+      System.out.println("충전 레벨 확인: " + chargeLevel);
+      System.out.println(model + "의 엔진을 구동합니다.");
+    }
   }
 }
